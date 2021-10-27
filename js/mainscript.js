9bfa082b6dca78e14f753152a3a22bfb9b769d53
Gideon <gideon.debruijn@student.hu.nl>
2021-10-27 15:20:54 +0200

    asd

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,29 +1,56 @@
+document.getElementById("pic1").style.display = "none";
+document.getElementById("pic2").style.display = "none";
+document.getElementById("pic3").style.display = "none";
+document.getElementById("pic4").style.display = "none";
 
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 function myFunction1() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  
-  setTimeout(carouselpic, 3000); // Change image every 2 seconds
+  var x = document.getElementById("pic1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    sleep(15000).then(() => {
+      x.style.display = "none";
+  });
+  } else {
+    x.style.display = "none";
+  }
 }
 
 function myFunction2() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  
-  setTimeout(carouselpic, 3000); // Change image every 2 seconds
+  var x = document.getElementById("pic2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    sleep(15000).then(() => {
+      x.style.display = "none";
+  });
+  } else {
+    x.style.display = "none";
+  }
 }
 
 function myFunction3() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  
-  setTimeout(carouselpic, 3000); // Change image every 2 seconds
+  var x = document.getElementById("pic3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    sleep(15000).then(() => {
+      x.style.display = "none";
+  });
+  } else {
+    x.style.display = "none";
+  }
 }
 
 function myFunction4() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  
-  setTimeout(carouselpic, 3000); // Change image every 2 seconds
+  var x = document.getElementById("pic4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    sleep(15000).then(() => {
+      x.style.display = "none";
+  });
+  } else {
+    x.style.display = "none";
+  }
 }
