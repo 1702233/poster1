29a94a26c551ce0d3d3f3aa2ffeb3bccc272ae88
Gideon <gideon.debruijn@student.hu.nl>
2021-10-27 14:55:11 +0200

    asd

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,28 +1,29 @@
-var slideIndex = 0;
-var slideIndex2 = 0;
-carouselpic();
-carousellove();
 
-function carouselpic() {
+
+function myFunction1() {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
+  
   setTimeout(carouselpic, 3000); // Change image every 2 seconds
 }
 
-function carousellove() {
+function myFunction2() {
   var i;
-  var x = document.getElementsByClassName("myLove");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  slideIndex2++;
-  if (slideIndex2 > x.length) {slideIndex2 = 1} 
-  x[slideIndex2-1].style.display = "block"; 
-  setTimeout(carousellove, 4000); // Change image every 2 seconds
+  var x = document.getElementsByClassName("mySlides");
+  
+  setTimeout(carouselpic, 3000); // Change image every 2 seconds
+}
+
+function myFunction3() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  
+  setTimeout(carouselpic, 3000); // Change image every 2 seconds
+}
+
+function myFunction4() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  
+  setTimeout(carouselpic, 3000); // Change image every 2 seconds
 }
